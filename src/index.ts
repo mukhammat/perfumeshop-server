27fce7eb402e8881fd9bbc8344bf8172c772a7c0
mukhammat <dosnet2200@gmail.com>
2025-01-17 17:28:09 +0300

    feat: add CORS support to the Express server and update package dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import router from "./routers";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use("/api", router);
 
 app.listen(port, () => {
