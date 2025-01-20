0dff588b12f26ac625b0e4b51e603f816db0f21b
mukhammat <dosnet2200@gmail.com>
2025-01-20 08:36:38 +0300

    feat: add JSDoc comments for jwtGenerate function in jwt.helper.ts

--- src/helpers/jwt.helper.ts
+++ src/helpers/jwt.helper.ts
@@ -6,6 +6,13 @@ interface JwtPayload {
 
 const secretKey = process.env.SECRET_KEY || "secretKey";
 
+/**
+ * Генерация JWT токена
+ * @param payload Полезная нагрузка токена
+ * @param expiresIn Время жизни токена
+ * @returns JWT токен
+ */
+
 export const jwtGenerate = (payload:JwtPayload, expiresIn = "24h") => {
     const token = jwt.sign(payload, secretKey, { expiresIn });
     return token;
